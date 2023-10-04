ea958cd24febdf19848b096420584325730dae5b
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-03 17:30:32 -0700

    testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,15 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    /**
+     * This is an empty function
+     */
+    public static void thisFunct(){
+
+    }
+
+    public static void emptyFunction(){
+
+    }
 }
